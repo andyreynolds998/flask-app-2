48c9dfe6048c05a789bf02017a0de74852eb4ead
Andrew <andyreynolds998@gmail.com>
2021-08-05 18:50:49 -0700

    first commit

--- app/routes.py
+++ app/routes.py
@@ -50,3 +50,9 @@ def get_single_user(uid):
     }
     out["body"] = select(uid)
     return out
+
+
+@app.route('/agent')
+def agent():
+    user_agent = request.headers.get("Users-Agent")
+    return "<p> Your user agent is: %s</p>" % user_agent
